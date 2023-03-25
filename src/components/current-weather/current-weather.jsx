6dee69bf144bf50e0af7c6cba6ef1b30ad9ffde3
Makhareishvili <levanimakharei7li@gmail.com>
2023-03-25 12:26:00 +0400

    Add animation to current weather component

--- src/components/current-weather/current-weather.jsx
+++ src/components/current-weather/current-weather.jsx
@@ -24,20 +24,24 @@ const Currentweather = ({ data }) => {
           </div>
           <div className="parameter-row">
             <span className="parameter-label">Feel like</span>
+            <div className="animation-div" />
             <span className="parameter-value">
               {Math.round(data.main.feels_like)}°C
             </span>
           </div>
           <div className="parameter-row">
             <span className="parameter-label">Wind</span>
+            <div className="animation-div" />
             <span className="parameter-value">{data.wind.speed} m/s</span>
           </div>
           <div className="parameter-row">
             <span className="parameter-label">Humidity</span>
+            <div className="animation-div" />
             <span className="parameter-value">{data.main.humidity}%</span>
           </div>
           <div className="parameter-row">
             <span className="parameter-label">Presure</span>
+            <div className="animation-div" />
             <span className="parameter-value">{data.main.pressure} hPa</span>
           </div>
         </div>
